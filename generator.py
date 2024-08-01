05933d1edbfe6615b2285c172caff7be9d13002b
Mohammad Reza <hoznaghashi@gmail.com>
2024-08-01 20:37:30 +0330

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 import numpy as np 
-def seq_generator(movie:np.array,running:np.array,trace:list,p,bsize:int,delay:int,shuffle = True):
+def seq_generator(movie:np.array,running:np.array,trace:list,p,bsize:int,delay:int,eye:np.ndarray,shuffle = True,affine=False):
    
     '''
     movie: numpy array with shape of (samples,h,w,c)
@@ -8,8 +8,9 @@ def seq_generator(movie:np.array,running:np.array,trace:list,p,bsize:int,delay:i
     p : list of indecies that used for given input domain i.e. natural movie
     bsize : batch size 
     delay: delay of input in compare to output 
+    eye: eye data
     shuffle: used for training of model, for inference purpose turn it False
-
+    affine : including affine transformation into the model and hence eyetracking data 
 
     return tupple : ([movie stimuli ,locmotion,trace past],trace)
 
@@ -19,15 +20,30 @@ def seq_generator(movie:np.array,running:np.array,trace:list,p,bsize:int,delay:i
     shape = p.shape
     movie_shape = movie.shape[1:]
     trace_shape = trace.shape[-1]
-    while True: 
-        if shuffle:
-            p = p.flatten() 
-            np.random.shuffle(p)
-            p = p.reshape(shape)
-        for s in p: 
-           t = trace[s]
-           get_idx=np.array([[x for x in range(o-delay,o)] for o in s]).flatten()
-           r = running[get_idx].reshape(bsize,delay)
-           out = movie[get_idx].reshape(bsize,delay,movie_shape[0],movie_shape[1],movie_shape[2])
-           feedback = trace[get_idx-1].reshape(bsize,delay,trace_shape)
-           yield ([out,r,feedback],t)
+    if affine:             
+        while True: 
+            if shuffle:
+                p = p.flatten() 
+                np.random.shuffle(p)
+                p = p.reshape(shape)
+            for s in p: 
+                t = trace[s]
+                get_idx=np.array([[x for x in range(o-delay,o)] for o in s]).flatten()
+                e = eye[get_idx].reshape((bsize,delay,4))
+                r = running[get_idx].reshape(bsize,delay)
+                out = movie[get_idx].reshape(bsize,delay,movie_shape[0],movie_shape[1],movie_shape[2])
+                feedback = trace[get_idx-1].reshape(bsize,delay,trace_shape)
+                yield ([out,r,feedback,e],t)
+    else: 
+        while True: 
+            if shuffle:
+                p = p.flatten() 
+                np.random.shuffle(p)
+                p = p.reshape(shape)
+            for s in p: 
+                t = trace[s]
+                get_idx=np.array([[x for x in range(o-delay,o)] for o in s]).flatten()
+                r = running[get_idx].reshape(bsize,delay)
+                out = movie[get_idx].reshape(bsize,delay,movie_shape[0],movie_shape[1],movie_shape[2])
+                feedback = trace[get_idx-1].reshape(bsize,delay,trace_shape)
+                yield ([out,r,feedback],t)
